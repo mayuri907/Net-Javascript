e15e22a3cc55d8bf63d4684f2d630dae2d76db99
mayuri907 <mayuribhivsane2000@gmail.com>
2026-04-07 23:56:33 +0530

    script3.js

---
+++ script3.js
@@ -0,0 +1,133 @@
+
+
+// let a = 10
+// console.log(a)
+// a  = 100
+// console.log(a)
+
+// const s = 1000
+// console.log(s)
+// //s = 1000
+
+
+// // Arithmetic operation 
+
+
+
+// let q = 6
+// let r = 3
+
+// console.log(q+r)
+// console.log(q-r)
+// console.log(q*r)
+// console.log(q/r)
+// console.log(q%r)
+
+// let i = 6
+// let k = 3
+
+// console.log(i+k)
+// console.log(i-k)
+// console.log(i*k)
+// console.log(i/k)
+// console.log(i%k)
+
+// // 1 pair -- 5 lines 
+// // 10 pair -- 50 lines
+
+// function Calculator(x,y){
+//     console.log(x+y)
+//     console.log(x-y)
+//     console.log(x*y)
+//     console.log(x/y)
+//     console.log(x%y)
+// }
+// Calculator(12,3)
+// Calculator(6,3)
+
+
+// day1 
+
+function addA(){
+    console.log(9+9)
+}
+addA()
+addA()
+addA()
+addA()
+
+function addB(x,y){
+    console.log()
+}
+addB(12,3)
+addB(12,1)
+addB(12,3)
+
+
+function addC(x,y){
+    return x + y
+}
+let q1 = addC(12,3)
+console.log(q1)
+console.log(q1 + q1)
+
+// Day 3
+
+
+
+// introvert      extrovert
+// calm             loud
+// less outing      more outing
+// less outin       more social
+
+// Human 
+// Propertiess - fn , ln , age , weight 
+// Methods - walk() , talk()
+
+// Vehicle 
+// Properties - color , type , logo , company
+// Methods -start() , stop()
+
+// Bank acc
+// Properties - accNo , IFSC code , bal , accName , type
+// Methods - deposit() , withdrawl()
+
+let a1  = 10 
+console.log(a1)
+console.log(typeof a1)
+// 10 , -10 , 0 , -10.5 , 10.55
+
+
+let a2 = true
+console.log(a2)
+console.log(typeof a2)
+// true false
+
+
+let a3 = "chinmay"
+console.log(a3)
+console.log(typeof a3)
+// "a","A","123","@#erdsdff$%#%"," "," 43535"
+
+// comparison operator
+
+// entity < entity ----> boolean --> true or false
+console.log(typeof 10) // number
+console.log(typeof '10') // string
+// absolute value is same 
+
+// <, > , <= , >= 
+// == , !=  -- value
+// === ,!== -- value and type
+console.log(2 === 2)
+console.log(2 === '2')
+console.log(2 !== '2')
+console.log(2 !== 2)
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 == 2)
+console.log(2 == 3)
+console.log(2 >= 1)
+console.log(2 <= 1)
+console.log(2 >= 2)
+console.log(2 <= 2)
